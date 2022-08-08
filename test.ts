8ac0b25bafe3cb770716c4792e9a47d8fae26f87
MartinGamesCZ <martinpetrnp@gmail.com>
2022-08-08 15:27:10 +0200

    idk

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
-import Easybase from ".";
-import { CONTAINS, ENDS_WITH, LIKE, STARTS_WITH } from "./operators";
+import Easybase from "./src";
+import { CONTAINS, ENDS_WITH, LIKE, STARTS_WITH } from "./src/operators";
 require("dotenv").config();
 
 var db = new Easybase({
@@ -8,10 +8,8 @@ var db = new Easybase({
     password: process.env.DB_PASS ?? ""
 })
 
-const conditions = {
-    id: 3
-}
-
-db.table("users").delete(conditions).then(console.log).catch(console.error)
+db.table("users").get({
+    name: STARTS_WITH("J")
+}).then(console.log).catch(console.error)
 
 setInterval(() => {}, 1000);
